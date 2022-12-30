6794beb8934af3b0263da905165042f591f1b7d2
Moises <moyses8a@gmail.com>
2022-12-30 16:14:17 -0600

    Clean code

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App({title}) {
   const [people, setPeople] = useState(data);
-  console.log('msg: ', title);
   return <main>
     <section className='container'>
       <h3>{people.length} Birthdays today</h3>
